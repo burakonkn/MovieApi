c23ce6e4289e36900054c2c41e1bece815b96d35
burakonkn <burakonkn@gmail.com>
2026-03-15 21:09:12 +0300

    GetCategoryQueryHandler ve GetMovieQueryHandler Sınıfı

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/GetMovieQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Application.Features.CQRSDesignPattern.Results.MovieResult;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class GetMovieQueryHandler
+    {
+        private readonly MovieContext _context;
+
+        public GetMovieQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<GetMovieQueryResult>> Handle()
+        {
+            var values = await _context.Movies.ToListAsync();
+            return values.Select(x => new GetMovieQueryResult
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Description = x.Description,
+                CoverImageUrl = x.CoverImageUrl,
+                CreatedYear = x.CreatedYear,
+                Duration = x.Duration,
+                Rating = x.Rating,
+                ReleaseDate = x.ReleaseDate,
+                Status = x.Status
+            }).ToList();
+        }
+    }
+}
