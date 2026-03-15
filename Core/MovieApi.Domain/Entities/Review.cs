d0bf2f137f5f80d48c6fc03360f2b2ba46425bad
burakonkn <burakonkn@gmail.com>
2026-03-15 17:09:03 +0300

    Domain Katmanı ve Entityler

---
+++ Core/MovieApi.Domain/Entities/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Domain.Entities
+{
+    public class Review
+    {
+        public Guid Id { get; set; }
+        public string Comment { get; set; }
+        public int Rating { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
