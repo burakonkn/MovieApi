c330da6f105c64e7e3a65b91a38389f0de2ac3cd
burakonkn <burakonkn@gmail.com>
2026-03-15 20:55:34 +0300

    Handler Metotlarının Yazılması

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/CreateMovieCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Domain.Entities;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class CreateMovieCommandHandler
+    {
+        private readonly MovieContext _context;
+
+        public CreateMovieCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async void Handle(CreateMovieCommand command)
+        {
+            var movie = new Movie()
+            {
+                Title = command.Title,
+                Description = command.Description,
+                ReleaseDate = command.ReleaseDate,
+                CoverImageUrl = command.CoverImageUrl,
+                CreatedYear = command.CreatedYear,
+                Duration = command.Duration,
+                Rating = command.Rating,
+                Status = command.Status
+            };
+
+            await _context.Movies.AddAsync(movie);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
