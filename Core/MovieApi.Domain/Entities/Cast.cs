90db79f41e96337e142bfca0e1f06031fddc6bb3
burakonkn <burakonkn@gmail.com>
2026-03-15 17:23:09 +0300

    Entitylerin Tamamlanması ve Persistence Katmanı

---
+++ Core/MovieApi.Domain/Entities/Cast.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Domain.Entities
+{
+    public class Cast
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string? Overview { get; set; }
+        public string? Biography { get; set; }
+    }
+}
---
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Persistence.Context
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=MovieApiDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Cast> Casts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
