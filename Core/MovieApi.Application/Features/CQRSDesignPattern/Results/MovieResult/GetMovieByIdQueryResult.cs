7d40e7a07e49a33ac55492b1b913731370f8567a
burakonkn <burakonkn@gmail.com>
2026-03-15 18:22:50 +0300

    Application Katmanı ve CQRS Tasarım Deseninin Uygulanması

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Results/MovieResult/GetMovieByIdQueryResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Results.MovieResult
+{
+    public class GetMovieByIdQueryResult
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string CoverImageUrl { get; set; } = string.Empty;
+        public decimal Rating { get; set; }
+        public string? Description { get; set; }
+        public int Duration { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string CreatedYear { get; set; } = string.Empty;
+        public bool Status { get; set; }
+    }
+}
