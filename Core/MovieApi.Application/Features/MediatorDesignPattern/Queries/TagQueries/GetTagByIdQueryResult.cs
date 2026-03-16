9c61cee3169f4061a94fc0432f84ccd7aa999b5d
burakonkn <burakonkn@gmail.com>
2026-03-16 15:18:30 +0300

    Cast Mediator Controller / Tag Query, Result İşlemleri

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Queries/TagQueries/GetTagByIdQueryResult.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Queries.TagQueries
+{
+    public class GetTagByIdQueryResult : IRequest<GetTagByIdQueryResult>
+    {
+        public GetTagByIdQueryResult(Guid id)
+        {
+            Id = id;
+        }
+        public Guid Id { get; set; }
+
+    }
+}
---
+++ Presentation/MovieApi.WebAPI/Controllers/CastsController.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.MediatorDesignPattern.Commands.CastCommands;
+using MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries;
+
+namespace MovieApi.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CastsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CastsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public IActionResult CastList()
+        {
+            var value = _mediator.Send(new GetCastQuery());
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCast(CreateCastCommand request)
+        {
+            _mediator.Send(request);
+            return Ok("Oyuncu ekleme işlemi başarılı.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCast(Guid id)
+        {
+            _mediator.Send(new RemoveCastCommand(id));
+            return Ok("Oyuncu silme işlemi başarılı.");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCast(UpdateCastCommand request)
+        {
+            _mediator.Send(request);
+            return Ok("Oyuncu güncelleme işlemi başarılı.");
+        }
+
+        [HttpGet("GetCast")]
+        public IActionResult GetCast(Guid id)
+        {
+            var value = _mediator.Send(new GetCastByIdQuery(id));
+            return Ok(value);
+        }
+    }
+}
