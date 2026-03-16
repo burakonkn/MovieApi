677da254e5aaa33364fa106bfb0b1f00f1085094
burakonkn <burakonkn@gmail.com>
2026-03-16 14:57:12 +0300

    Mediator Handler Sınıfları

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/CreateCastCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Commands.CastCommands;
+using MovieApi.Domain.Entities;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
+{
+    public class CreateCastCommandHandler : IRequestHandler<CreateCastCommand>
+    {
+        private readonly MovieContext _context;
+
+        public CreateCastCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(CreateCastCommand request, CancellationToken cancellationToken)
+        {
+            await _context.Casts.AddAsync(new Cast
+            {
+                Name = request.Name,
+                Surname = request.Surname,
+                Biography = request.Biography,
+                ImageUrl = request.ImageUrl,
+                Overview = request.Overview,
+                Title = request.Title
+            });
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/UpdateCastCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Commands.CastCommands;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
+{
+    public class UpdateCastCommandHandler : IRequestHandler<UpdateCastCommand>
+    {
+        private readonly MovieContext _context;
+
+        public UpdateCastCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(UpdateCastCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _context.Casts.FindAsync(request.Id);
+            value.Name = request.Name;
+            value.Surname = request.Surname;
+            value.Overview = request.Overview;
+            value.Biography = request.Biography;
+            value.ImageUrl = request.ImageUrl;
+            value.Title = request.Title;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
