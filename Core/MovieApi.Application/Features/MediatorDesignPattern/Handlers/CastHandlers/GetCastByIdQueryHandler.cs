50879f905cfb97d625b61644be23174ae34fb568
burakonkn <burakonkn@gmail.com>
2026-03-16 15:06:33 +0300

    Mediator Cast Query Handler

--- Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/GetCastByIdQueryHandler.cs
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/GetCastByIdQueryHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.CastResults;
+using MovieApi.Persistence.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,28 @@ using System.Threading.Tasks;
 
 namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
 {
-    internal class GetCastByIdQueryHandler
+    public class GetCastByIdQueryHandler : IRequestHandler<GetCastByIdQuery, GetCastByIdQueryResult>
     {
+        private readonly MovieContext _context;
+
+        public GetCastByIdQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetCastByIdQueryResult> Handle(GetCastByIdQuery request, CancellationToken cancellationToken)
+        {
+            var value = await _context.Casts.FindAsync(request.Id);
+            return new GetCastByIdQueryResult
+            {
+                Id = value.Id,
+                Name = value.Name,
+                Surname = value.Surname,
+                Title = value.Title,
+                Biography = value.Biography,
+                Overview = value.Overview,
+                ImageUrl = value.ImageUrl
+            };
+        }
     }
 }
